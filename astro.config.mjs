473747d0c65a2c86c80fb08a1aed9bc5c5b8b12b
Ocean1029 <vanessa0201029@gmail.com>
2026-08-02 02:32:36 +0800

    Fix GitHub Pages asset paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://ocean1029.com',
+  base: process.env.GITHUB_ACTIONS ? '/ocean-personal-site' : undefined,
   integrations: [sitemap()],
   i18n: {
     defaultLocale: 'zh-tw',
